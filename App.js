bb7822893079bcec8c1c65e58f5491542f0b9f16
abhisheksati51 <abhisheksati51@gmail.com>
2024-02-21 09:29:44 +0530

    first project

---
+++ App.js
@@ -0,0 +1,28 @@
+// const heading = React.createElement(
+//   "h1",
+//   {id:'heading'},
+//   "Hello everyone this is my first react Project"
+// );
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div",
+   { id: "child" },
+   [
+    React.createElement('h1',{},"This is heading 1"),
+    React.createElement('h2',{},"This is heading 2"),
+   ]
+   ),
+   React.createElement("div",
+   { id: "child1" },
+   [
+    React.createElement('h1',{},"This is heading 1"),
+    React.createElement('h2',{},"This is heading 2"),
+   ]
+   )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
